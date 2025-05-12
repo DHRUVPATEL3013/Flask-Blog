9d23ce71a5c69090871a90ca63e202d5a63d2e29
ROLEX3013 <dhrpatel2833@gmail.com>
2025-05-12 11:22:21 +0530

    modified utils.py file and run.py file

--- flaskblog/users/utils.py
+++ flaskblog/users/utils.py
@@ -5,6 +5,7 @@ from flask import url_for,current_app
 from flaskblog import db,bcrypt,mail
 from flask_mail import Message
 
+
 def save_picture(form_picture):
     random_hex=secrets.token_hex(8)
     _,f_ext=os.path.splitext(form_picture.filename)
@@ -17,10 +18,11 @@ def save_picture(form_picture):
 
     return picture_name
 
+SENDER=os.getenv("MAIL_USERNAME")
 def send_reset_email(user):
     token=user.get_reset_token()
-    reset_url = f"http://192.168.182.36:5000" + url_for('users.reset_token', token=token, _external=False)
-    msg=Message('Password Reset Request',sender='dhrptl714@gmail.com   ',recipients=[user.email])
+    reset_url = url_for('users.reset_token', token=token, _external=True)
+    msg=Message('Password Reset Request',sender=SENDER,recipients=[user.email])
     msg.body=f"""
     To reset your password,visit the following link: 
 {reset_url}
